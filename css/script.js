fa64a1cce7dfb69f8ef1451d22e4454ebc39590a
hoangquandev <lehoangquanit@gmail.com>
2022-05-03 18:12:20 +0700

    upload

---
+++ css/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:["","bà con ơi"],
+    typeSpeed: 200,
+    BackSpeed: 60,
+    loop:true
+})
+var typed = new Typed(".typing1",{
+    strings:["","con bà bảy"],
+    typeSpeed: 200,
+    BackSpeed: 60,
+    loop: true
+})
