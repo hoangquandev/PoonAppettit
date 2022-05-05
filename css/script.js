b037d3ead472c0bfc7d4df0ab422235c93d9b5eb
hoangquandev <lehoangquanit@gmail.com>
2022-05-05 18:51:29 +0700

    responsive and header

--- css/script.js
+++ css/script.js
@@ -13,7 +13,9 @@ var typed = new Typed(".typing1",{
 
 // hamburger
 const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
 
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
 });
